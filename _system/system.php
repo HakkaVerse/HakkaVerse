041dbe2a752ea78f5b4d27b24c55e769110899f9
HakkaVerse <104783861+HakkaVerse@users.noreply.github.com>
2022-05-22 21:11:09 +0700

    Update system.php

--- _system/system.php
+++ _system/system.php
@@ -59,4 +59,9 @@ function thai_date_and_time($time){
     return $thai_date_return;
 }
 
+function clean($string) {
+    $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+ 
+    return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+ }
 account()->CheckUser();
